f32d2b06dfd53eb18d69d58c5534ed8d5f7fd82c
namotoy <namotopet0615@gmail.com>
2021-11-13 10:45:27 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,11 +1,11 @@
 function post (){
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e)=> {
+  submit.addEventListener("click", (e) => {
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST","/posts", true);
+    XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
   });
